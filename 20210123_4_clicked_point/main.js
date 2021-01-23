b2ab086dd8369cc030e8c5fa4f599eccc8156a99
yo16 <xxyo1xx@gmail.com>
2021-01-23 14:19:32 +0900

    座標値を表示する関数をcommon化

--- 20210123_4_clicked_point/main.js
+++ 20210123_4_clicked_point/main.js
@@ -4,19 +4,3 @@ $(document).ready(function(){
 
     add_clicked_pos_event($("#svg1"));
 });
-
-// クリックした位置に点と座標値を描く、クリックイベントを追加
-function add_clicked_pos_event(elm){
-    elm.click(function(e){
-        elm.append($(circle(
-            e.offsetX, e.offsetY,
-            5, "#f00",1, "#fcc"
-        )));
-
-        elm.append($(text(
-            e.offsetX, e.offsetY,
-            "("+e.offsetX+", "+e.offsetY+")",16
-        )));
-    });
-}
-
