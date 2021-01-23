1959d75e67f13f89ea25a3c4360c9f817fbc3ca9
yo16 <xxyo1xx@gmail.com>
2021-01-23 10:08:59 +0900

    transformのテスト

---
+++ 20210123_1_transform_matrix/main.js
@@ -0,0 +1,8 @@
+var pos = [0,0];
+var offset_pos = [0,0];
+var is_dragging = false;
+
+$(document).ready(function(){
+    $("#svg1").css("background-color", "#ccc");
+    $("#svg2").css("background-color", "#ccc");
+});
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # svg_control
 SVG要素を作ったりドラッグしたり早くしたりする試行錯誤
+
+## SVG仕様
+https://triple-underscore.github.io/SVG11/
+
+## next memo
+- transform.matrix上で変形した後にクリックしたときの座標値は、正しく取れるのか確認
+- ドラッグでmatrixを変更する
